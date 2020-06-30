44169d1a8c88451f9058162985b48349606c22ad
Janek Mangold <janek.mangold@web.de>
2020-06-30 20:41:07 +0000

    Update regex_operations.py

--- prime_league/regex_operations.py
+++ prime_league/regex_operations.py
@@ -4,13 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 from patterns import LOGS, SUMMONER_NAMES, ENEMY_TEAM_ID, GAME_DAY, TEAM_NAME, MATCH_IDS
 
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Max-Age': '3600',
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-}
 
 TEAMS_URL = "https://www.primeleague.gg/de/leagues/teams/"
 MATCHES_URL = "https://www.primeleague.gg/de/leagues/matches/"
