2170f566be30fc90b8cb09ded013aec955a10a13
OrbisK <r-kehl@gmx.de>
2020-07-08 23:09:09 +0200

    removed team id from .env

--- app_prime_league/migrations/0002_auto_20200706_0200.py
+++ app_prime_league/migrations/0002_auto_20200706_0200.py
@@ -4,15 +4,7 @@ from prime_league_bot import settings
 
 
 def add_seeds(apps, schema_editor):
-
-    if settings.DEFAULT_TEAM_ID is not None:
-        pass
-        # Team = apps.get_model('app_prime_league', 'Team')
-        # team = Team(
-        #     id=settings.DEFAULT_TEAM_ID,
-        #     telegram_channel_id=settings.DEFAULT_TELEGRAM_CHAT_ID
-        # )
-        # team.save()
+    pass
 
 
 class Migration(migrations.Migration):
