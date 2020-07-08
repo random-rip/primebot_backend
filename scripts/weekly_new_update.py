13001589c361ce7aa13779c2b0fc6ea5dcec9d98
OrbisK <r-kehl@gmx.de>
2020-07-09 00:42:01 +0200

    hardcoded weekly newupdate

--- scripts/weekly_new_update.py
+++ scripts/weekly_new_update.py
@@ -1,12 +1,19 @@
+from datetime import datetime
+
+import pytz
+
 from app_prime_league.models import Game
 from telegram_interface.tg_singleton import TelegramMessagesWrapper
 
 
 def main():
-    game_day = 6
+    start_date = datetime(2020, 6, 8).astimezone(pytz.timezone("Europe/Berlin"))
+    current_date = datetime.now().astimezone(pytz.timezone("Europe/Berlin"))
+    game_day = ((current_date - start_date) / 7).days + 1
     games = Game.objects.filter(game_day=game_day, game_closed=False)
     for i in games:
-        if i.team.setting_set.get("weekly_op_link", True):
+        settings = dict(i.team.setting_set.all().values_list("attr_name", "attr_value"))
+        if settings.get("weekly_op_link", True):
             TelegramMessagesWrapper.send_new_game_day(i)
 
 
