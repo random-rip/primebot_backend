df4d13f908a25f909b76b2ded84c83509e14b40f
OrbisK <r-kehl@gmx.de>
2020-07-11 22:52:09 +0200

    Setup for global message

--- app_prime_league/views.py
+++ app_prime_league/views.py
@@ -1,5 +1,7 @@
 import datetime
 
+import telepot
+from django.db import models
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -7,8 +9,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 
+from app_prime_league.models import Team
+from telegram_interface import send_message
 
-class GlobalMessage(LoginRequiredMixin, TemplateView, ):
+
+class GlobalMessage(TemplateView, LoginRequiredMixin, ):
     template_name = 'global_message.html'
 
     def get(self, request, *args, **kwargs):
@@ -17,5 +22,7 @@ class GlobalMessage(LoginRequiredMixin, TemplateView, ):
 
     def post(self, request, *args, **kwargs):
         msg = request.POST["message"]
-        print(msg)
+        teams = Team.objects.exclude(telegram_channel_id__isnull=True)
+        for team in teams:
+            send_message(msg=msg, chat_id=team.telegram_channel_id)
         return render(request, self.template_name)
