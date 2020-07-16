45d40dcf3844b7e70093aed89368834666a90f04
OrbisK <r-kehl@gmx.de>
2020-07-16 02:29:22 +0200

    small changes

--- telegram_interface/botfather.py
+++ telegram_interface/botfather.py
@@ -89,6 +89,12 @@ def explain(update: Update, context: CallbackContext):
 
 # /settings
 def start_settings(update: Update, context: CallbackContext):
+    chat_type = update["message"]["chat"]["type"]
+    if chat_type != "group":
+        return wrong_chat_type(update, context)
+    team = update_team(update["message"]["chat"]["id"], settings={})
+    if team is None:
+        return team_not_exists(update, context)
     update.message.reply_text(
         SETTINGS_MAIN_MENU["text"],
         reply_markup=main_menu_keyboard(),
@@ -132,6 +138,24 @@ def get_team_id(update: Update, context: CallbackContext):
             disable_web_page_preview=True,
         )
         return ConversationHandler.END
+
+
+def team_not_exists(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        text=TEAM_NOT_IN_DB_TEXT,
+        chat_id=update["message"]["chat"]["id"],
+        parse_mode="Markdown",
+    )
+    return ConversationHandler.END
+
+
+def wrong_chat_type(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        text=NO_GROUP_CHAT,
+        chat_id=update["message"]["chat"]["id"],
+        parse_mode="Markdown",
+    )
+    return ConversationHandler.END
 
 
 ############################ Settings Main #########################################
