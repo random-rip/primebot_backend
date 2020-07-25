8e03ac41ad0ff823c0b1ec4c31ffbc2f8e05f22d
Janek Mangold <janek.mangold@web.de>
2020-07-25 02:21:41 +0200

    added try pass method to error callback, added message to debug

--- scripts/debug.py
+++ scripts/debug.py
@@ -1,38 +1,19 @@
-from app_prime_league.models import Game
-from telegram_interface.tg_singleton import TelegramMessagesWrapper
+from app_prime_league.models import Game, Team
+from telegram_interface.tg_singleton import TelegramMessagesWrapper, send_message
 
 
 def main():
-    match = Game.objects.filter(game_id=596473).first()
+    pattern = "Hallo Beschwörer,\n" \
+              "ich war leider down #rip, aber jetzt gehts wieder (hoffentlich).\n" \
+              "Aufgrund von Serverfehlern war ich am 24.07.2020 nicht vollständig erreichbar." \
+              "Bitte versucht noch einmal, euer Team zu initialisieren (/start)." \
+              "Die Registrierung ist in der Regel nach maximal 30 Sekunden abgeschlossen. " \
+              "Sollte das bei euch nicht der Fall sein, schreibt bitte direkt meine Entwickler an (@Grayknife).\n" \
+              "Liebe Grüße\n" \
+              "_Grayknife und Orbis_"
 
-    print(match.get_op_link_of_enemies(only_lineup=False))
-
-    TelegramMessagesWrapper.send_new_lineup_of_enemies(match)
-    # pattern = "Hallo {team_tag},\n" \
-    #       "hier ein kurzes Update der letzten größeren Änderungen am Bot.\n\n" \
-    #       "*Neuer Command:* \n" \
-    #       "- /setlogo - setzt euer Telegram Gruppenbild auf das bei der PrimeLeague hinterlegte Foto. " \
-    #       "(Der Bot benötigt dafür Adminrechte)\n\n" \
-    #       "*Überarbeiteter Command:* \n" \
-    #       "- /start - startet die Registirieung eines Teams. Hierbei wurden die Abfrage nach der TeamID angepasst und " \
-    #       "der Bot fragt anschließend nach der Übernahme des Gruppenbildes. \n" \
-    #       "- /settings - lässt euch weiterhin die Settings für euren Bot einstellen, jedoch wurde die Menüführung " \
-    #       "verändert. Schaut es euch gern an. Die Einstellungen lassen sich nun bequem über ein übersichtliches Menü " \
-    #       "einstellen. Weitere Optionen sind in Planung. Ihr könnt uns gerne /feedback geben.\n\n" \
-    #       "*Bugfixes:* \n" \
-    #       "1️⃣ Ein Fehler wurde behoben, durch den Teams, die sich registriert haben, keine Bestätigungsmeldung " \
-    #       "bekommen haben!\n" \
-    #       "2️⃣ Ein Fehler wurde behoben, durch den Spiele, die aufgrund von lineup\_notready beendet wurden, nicht als " \
-    #       "gespielt markiert wurden.\n" \
-    #       "3️⃣ Kleinere Fehler wurden behoben.\n" \
-    #       "Sollte ihr einen Fehler bemerken, nutzt bitte /issue.\n\n" \
-    #       "*Coming Soon:*\n" \
-    #       "1️⃣ Vollständige Integration für Teams, die sich noch in der Swiss Starter Kalibrierungsphase befinden.\n" \
-    #       "2️⃣ Anpinnen der Nachricht mit dem nächsten Spieltag.\n" \
-    #       "3️⃣ Play-Off Integration. Möglicherweise noch nicht vollständig für diese Play-Offs.\n\n" \
-    #       "Schreibt uns gerne, sollte euch noch etwas einfallen (/feedback).\n\n" \
-    #       "Liebe Grüße\n" \
-    #       "_Grayknife und Orbis_"
+    print(send_message(msg=pattern, chat_id=-490819576))
+    print(send_message(msg=pattern, chat_id=-373994529))
 
 
 def run():
--- telegram_interface/botfather.py
+++ telegram_interface/botfather.py
@@ -23,32 +23,38 @@ def error(update, context):
     # we want to notify the user of this problem. This will always work, but not notify users if the update is an
     # callback or inline query, or a poll update. In case you want this, keep in mind that sending the message
     # could fail
-    if update.effective_message:
-        text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
-               "My developer(s) will be notified."
-        update.effective_message.reply_text(text)
-    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
-    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
-    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
-    # empty string works fine.
-    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
-    # lets try to get as much information from the telegram update as possible
-    payload = ""
-    # normally, we always have an user. If not, its either a channel or a poll update.
-    if update.effective_user:
-        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
-    # there are more situations when you don't get a chat
-    if update.effective_chat:
-        payload += f' within the chat <i>{update.effective_chat.title}</i>'
-        if update.effective_chat.username:
-            payload += f' (@{update.effective_chat.username})'
-    # but only one where you have an empty payload by now: A poll (buuuh)
-    if update.poll:
-        payload += f' with the poll id {update.poll.id}.'
-    # lets put this in a "well" formatted text
-    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
-           f"</code>"
-    # and send it to the dev(s)
+    try:
+        if update is not None:
+            if update.effective_message:
+                text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
+                       "My developer(s) will be notified."
+                update.effective_message.reply_text(text)
+            # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
+            # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
+            # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
+            # empty string works fine.
+            trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+            # lets try to get as much information from the telegram update as possible
+            payload = ""
+            # normally, we always have an user. If not, its either a channel or a poll update.
+            if update.effective_user:
+                payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+            # there are more situations when you don't get a chat
+            if update.effective_chat:
+                payload += f' within the chat <i>{update.effective_chat.title}</i>'
+                if update.effective_chat.username:
+                    payload += f' (@{update.effective_chat.username})'
+            # but only one where you have an empty payload by now: A poll (buuuh)
+            if update.poll:
+                payload += f' with the poll id {update.poll.id}.'
+            # lets put this in a "well" formatted text
+            text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+                   f"</code>"
+            # and send it to the dev(s)
+        else:
+            text = "Ein gravierender Fehler ist aufgetreten (update is none)."
+    except Exception as e:
+        text = f"Ein gravierender Fehler ist aufgetreten.\n{e}"
     for dev_id in devs:
         context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
     # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
