f59b5a513c2680f9c10716e43d00d26af3813b0e
Janek Mangold <jmangold@singular-it.de>
2020-09-27 21:38:28 +0200

    asd

--- scripts/new_season_message.py
+++ scripts/new_season_message.py
@@ -6,24 +6,13 @@ from utils.constants import EMOJI_ONE, EMOJI_MINDBLOWN, EMOJI_SOON, EMOJI_ARROW,
 
 def main():
     # teams = Team.objects.exclude(telegram_id__isnull=True, division__isnull=True, id=89678)
-    teams = Team.objects.filter(id__in=[93008, 101916, 111914, 119671])
+    teams = Team.objects.filter(telegram_id__isnull=False, division__isnull=False)
+    print(teams)
     for team in teams:
         print(team)
-        next_match = team.games_against.order_by("game_begin").first()
-        pattern = "Hallo Beschwörer, \n \n" \
-                  "der Wintersplit 2020 ❄ beginnt morgen und ich bin natürlich auch am Stizzle. \n" \
-                  f"In diesem Chat wurde folgendes Team registriert: {team.name} \n" \
-                  f"Ihr spielt in der Division {team.division} und euer nächstes " \
-                  f"Match {EMOJI_SOON} bestreitet ihr gegen {EMOJI_ARROW}[{next_match.enemy_team.name}]({GENERAL_MATCH_LINK}{next_match.game_id}). \n\n" \
-                  "*Hinweis:* \n" \
-                  "Wenn es für dieses Team inzwischen eine neue Chatgruppe gibt, " \
-                  "könnt ihr dieses mit /reassign in der neuen Gruppe initialisieren. \n\n" \
-                  "*Coming Soon:* \n" \
-                  f"{EMOJI_ONE} Discord-Bot Integration {EMOJI_MINDBLOWN}\n\n" \
-                  f"Viel Erfolg auf den Richtfeldern! {EMOJI_FIGHT}\n" \
-                  "Sternige Grüße \n" \
-                  "@Grayknife _und_ @OrbisK"
-
+        next_match = team.games_against.order_by("game_day").first()
+        pattern = f"\*Das nächste Match ist natürlich gegen " \
+                  f"{EMOJI_ARROW}[{next_match.enemy_team.name}]({GENERAL_MATCH_LINK}{next_match.game_id}). 😇"
         print(send_message(msg=pattern, chat_id=team.telegram_id))
 
 
