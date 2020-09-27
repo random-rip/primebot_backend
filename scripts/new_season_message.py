cd01dfadff7048627123ab2e38f42e0605d1c170
Janek Mangold <jmangold@singular-it.de>
2020-09-27 21:49:13 +0200

    remove prints

--- scripts/new_season_message.py
+++ scripts/new_season_message.py
@@ -7,13 +7,12 @@ from utils.constants import EMOJI_ONE, EMOJI_MINDBLOWN, EMOJI_SOON, EMOJI_ARROW,
 def main():
     # teams = Team.objects.exclude(telegram_id__isnull=True, division__isnull=True, id=89678)
     teams = Team.objects.filter(telegram_id__isnull=False, division__isnull=False)
-    print(teams)
     for team in teams:
-        print(team)
         next_match = team.games_against.order_by("game_day").first()
         pattern = f"\*Das nächste Match ist natürlich gegen " \
                   f"{EMOJI_ARROW}[{next_match.enemy_team.name}]({GENERAL_MATCH_LINK}{next_match.game_id}). 😇"
-        print(send_message(msg=pattern, chat_id=team.telegram_id))
+
+        send_message(msg=pattern, chat_id=team.telegram_id)
 
 
 def run():
