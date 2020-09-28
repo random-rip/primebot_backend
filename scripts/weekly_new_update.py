ae26d98127b1f1058f8973f6aedf46218f07ddbd
Janek Mangold <jmangold@singular-it.de>
2020-09-28 14:31:12 +0200

    Adjust weekly new update

--- scripts/weekly_new_update.py
+++ scripts/weekly_new_update.py
@@ -1,15 +1,20 @@
-from app_prime_league.models import Game
+from app_prime_league.models import Game, Team
 from telegram_interface.tg_singleton import TelegramMessagesWrapper
 from utils.utils import current_game_day
 
 
 def main():
     game_day = current_game_day()
-    games = Game.objects.filter(game_day=game_day, game_closed=False)
-    for i in games:
-        settings = dict(i.team.setting_set.all().values_list("attr_name", "attr_value"))
+
+    teams = Team.objects.filter(telegram_id__isnull=False)
+
+    for team in teams:
+        print(team)
+        settings = dict(team.setting_set.all().values_list("attr_name", "attr_value"))
         if settings.get("weekly_op_link", True):
-            TelegramMessagesWrapper.send_new_game_day(i, settings.get("pin_weekly_op_link", True))
+            next_match = team.games_against.filter(game_day=game_day).first()
+            print(next_match)
+            TelegramMessagesWrapper.send_new_game_day(next_match, settings.get("pin_weekly_op_link", True))
 
 
 def run():
