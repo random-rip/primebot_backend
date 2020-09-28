b071b93c55b5daaeb08eb8be5ee5046ae3e98704
Janek Mangold <jmangold@singular-it.de>
2020-09-28 14:06:25 +0200

    Replace last split start with new split start

--- scripts/weekly_new_update.py
+++ scripts/weekly_new_update.py
@@ -4,7 +4,7 @@ from utils.utils import current_game_day
 
 
 def main():
-    game_day = 7  # current_game_day()
+    game_day = current_game_day()
     games = Game.objects.filter(game_day=game_day, game_closed=False)
     for i in games:
         settings = dict(i.team.setting_set.all().values_list("attr_name", "attr_value"))
