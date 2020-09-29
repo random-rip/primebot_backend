ec2fd86f4b631c642216af379dafb80be624d646
OrbisK <r-kehl@gmx.de>
2020-09-29 17:50:28 +0200

    adding changelog dic

---
+++ utils/changelogs.py
@@ -0,0 +1,22 @@
+from utils.constants import EMOJI_ONE, EMOJI_TWO, EMOJI_ARROW_RIGHT
+
+CHANGELOGS = {
+    "1.12.0-beta": "Hallo {team.name},\n"
+                   "hier ein kurze Zusammenfassung der letzten größeren Änderungen am PrimeBot.\n"
+                   "...Janeks kreativer Text...\n"
+                   "*Updates: (1.12.0-beta)*\n"
+                   f"{EMOJI_ONE} /reassign ist jetzt in /start integriert. Falls ihr also ein *anderes* Team für diesen "
+                   "Chat registieren möchtet, führt einfach /start aus. (Hinweis: Es kann nur ein Team pro Chat"
+                   "und ein Chat pro Team registriert werden.)\n"
+                   f"{EMOJI_TWO} um ein Team in einem anderen Chat neu zu registrieren muss unter /settings {EMOJI_ARROW_RIGHT} "
+                   "Team-Sperre dein Team vorher freigeben.\n\n"
+                   "*Bugfixes:*\n"
+                   "Ein kleiner Bug wurde behoben, bei dem nicht angezeigt wurde, wenn eine Nachricht nicht angeheftet "
+                   "werden konnte.\n\n"
+                   "*Coming Soon:*\n"
+                   f"{EMOJI_ONE} Pushnachrichten für neue Kommentare in ungespielten Matches\n"
+                   f"{EMOJI_TWO} Discord-Bot\n\n"
+                   "Wenn ihr keine Patch-Notes erhalten wollt deaktiviert Bot-Patches in /settings\n\n"
+                   "Sternige Grüße\n"
+                   "@Grayknife und @OrbisK"
+}
