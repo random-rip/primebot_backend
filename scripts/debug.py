026547cfa684eaaf9c56d016bf91f28c692405cc
Janek Mangold <janek.mangold@web.de>
2020-11-01 13:43:51 +0100

    asd

--- scripts/debug.py
+++ scripts/debug.py
@@ -1,12 +1,15 @@
-from app_prime_league.models import Team
-from parsing.parser import MatchWrapper
+from communication_interfaces import send_message
+from utils.constants import EMOJI_CLOVER, EMOJI_PEACE
 
 
 def main():
-    team = Team.objects.get_team(111464)
-    wrapper = MatchWrapper(650878, team)
-    parser = wrapper.parser
-    print(parser.get_comments())
+    print(
+        send_message(
+            msg=f"Hi Chat,\nich war leider down, #rip.\nAber jetzt geht es wieder (hoffentlich). {EMOJI_CLOVER} \n"
+                f"Ihr könnt mich jetzt wieder in vollem Umfang benutzen! {EMOJI_PEACE}",
+            chat_id=913800738,
+        )
+    )
 
 
 def run():
