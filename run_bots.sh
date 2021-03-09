e9f7ec8ec975d1d86d97508c2cad57e747a13c29
Janek Mangold <janek.mangold@web.de>
2021-03-09 18:27:05 +0100

    asd

--- run_bots.sh
+++ run_bots.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /root/prime_league_bot && venv/bin/python manage.py runscript run_discord_bot & venv/bin/python manage.py runscript run_telegram_bot
+cd /root/prime_league_bot && venv/bin/python manage.py runscript run_discord_bot & venv/bin/python manage.py runscript run_telegram_bot &
