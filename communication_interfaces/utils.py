bb5da5cf0ce2543a358248b41e4bd52f086ea8bf
Janek Mangold <janek.mangold@web.de>
2021-03-17 16:24:06 +0100

    finally

--- communication_interfaces/utils.py
+++ communication_interfaces/utils.py
@@ -13,10 +13,12 @@ def mysql_has_gone_away_decorator(fn):
     :param fn:
     :return:
     """
+
     def wrapper(*args, **kwargs):
         print("YUhu wir sind hieR")
         async_to_sync(mysql_has_gone_away)()
         return fn(*args, **kwargs)
+
     return wrapper
 
 
@@ -27,3 +29,5 @@ async def mysql_has_gone_away(*args):
         log_text = f"{e}: TRY ESTABLISH NEW CONNECTION"
         logging.getLogger("commands_logger").info(log_text)
         db.close_old_connections()
+    finally:
+        return True
