fc83b8165ddceeaedade29f28eaf599ef2f00e83
Janek Mangold <janek.mangold@web.de>
2021-05-29 10:41:44 +0200

    asd

--- restart_bots.sh
+++ restart_bots.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-pkill -15 -f "venv/bin/python manage.py runscript run"
+pkill -f "venv/bin/python manage.py runscript run"
 wait
+sleep 2
 cd /root/prime_league_bot/ || exit
 wait
 venv/bin/python manage.py runscript run_discord_bot &
