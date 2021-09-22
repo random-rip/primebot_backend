c2aee6c3fe2f3978a59e9211186ff501ac1c2fdb
Janek Mangold <janek.mangold@web.de>
2021-09-23 00:37:43 +0200

    asd

--- shell_scripts/daily_team_updates.sh
+++ shell_scripts/daily_team_updates.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /root/prime_league_bot && venv/bin/python manage.py runscript team_updates >> logs/daily_team_updates.log
+cd /root/prime_league_bot && venv/bin/python manage.py runscript team_updates &
