f435019f8734b35aee4726ebe1d433a6662a0ae1
Janek Mangold <janek.mangold@web.de>
2022-01-17 22:30:58 +0100

    Update Changelogs

--- utils/changelogs.py
+++ utils/changelogs.py
@@ -1,38 +1,20 @@
-from utils.emojis import EMOJI_ONE, EMOJI_TWO, EMOJI_ARROW_RIGHT, EMOJI_POST_HORN, EMOJI_PEACE, EMOJI_PARTY2
-
 CHANGELOGS = {
     1: {
         "version": "1.12.0-beta",
         "text":
             """
-            Hallo {team.name}, 
-            hier ein kurze Zusammenfassung der letzten Änderungen am PrimeBot.
-            
-            *Updates ({version}):*"""
-            f"""
-{EMOJI_ONE} /reassign : Dieses Kommando ist jetzt in /start integriert. Falls ihr also ein *anderes* Team für diesen Chat registrieren möchtet, führt /start aus. 
-{EMOJI_TWO} Team-Sperre: Um ein Team in einem anderen Chat neu zu registrieren muss unter /settings {EMOJI_ARROW_RIGHT} Team-Sperre dein Team vorher freigegeben werden.
-
-*Bugfixes:*
+Das Kommando `/reassign` ist jetzt in /start integriert. 
+Um ein Team in einem anderen Chat neu zu registrieren muss unter /settings->Team-Sperre das Team vorher freigegeben werden.
 Ein Fehler wurde behoben, bei dem nicht angezeigt wurde, wenn eine Nachricht nicht angeheftet werden konnte.
-
-*Coming Soon:*
-{EMOJI_ONE} Pushnachrichten für neue Kommentare in ungespielten Matches
-{EMOJI_TWO} Discord-Bot
-
-Wenn ihr keine Patchnotes erhalten wollt, deaktiviert die Einstellung in /settings {EMOJI_ARROW_RIGHT} Bot Patches.
-
-Sternige Grüße
-@Grayknife und @OrbisK
 """
     },
     2: {
         "version": "1.12.1-beta",
-        "text": f"Supergroup Hotfix"
+        "text": f"Supergroup Hotfix."
     },
     3: {
         "version": "1.12.2-beta",
-        "text": f"My SQL Has Gone Away Hotfix"
+        "text": f"My SQL Has Gone Away Hotfix."
     },
     4: {
         "version": "1.12.3-beta",
@@ -41,20 +23,7 @@ Sternige Grüße
     },
     5: {
         "version": "1.13.0-beta",
-        "text":
-            """
-            Hallo {team.name}, 
-            """
-            f"""
-der Spring Split 2021 steht in den Startlöchern und der Primebot ist natürlich wieder am Start!
-Aufgrund der erhöhten Anzahl an Teams, die den Bot jetzt benutzen, kam es in den letzten Tagen leider zu einigen Ausfällen.
-Jedes Team, welches diese Nachricht erhält, ist auf jeden Fall erfolgreich in der Datenbank hinterlegt worden. Bei Problemen oder Fragen benutzt /issue.
-Allerdings können erst ab dem 29.01.2021 Benachrichtigungen{EMOJI_POST_HORN} bei Terminvorschlägen (o.ä.) gesendet werden.
-Wir arbeiten außerdem daran, dass ihr mehr Informationen aus unserer Datenbank abfragen könnt. {EMOJI_PEACE}
-
-Viel Erfolg im Split und sternige Grüße
-Grayknife und Orbis
-"""
+        "text": "Bugfix"
     },
     6: {
         "version": "1.13.1-beta",
@@ -62,116 +31,53 @@ Grayknife und Orbis
     },
     7: {
         "version": "1.13.2-beta",
-        "text": "Matches, in denen die Zeit von einem Administrator manuell eingestellt wurde, werden nun korrekt im System gespeichert und es werden nicht mehr mehrere Benachrichtigungen dazu gesendet."
+        "text": "Spiele, in denen die Zeit von einem Administrator manuell eingestellt wurde, werden nun korrekt "
+                "im System gespeichert und es werden nicht mehr mehrere Benachrichtigungen dazu gesendet."
     },
     8: {
         "version": "1.14.0-beta",
         "text":
             """
-            Hallo {team.name}, 
-            hier ein kurze Zusammenfassung der Neuerungen am PrimeBot.
-            
-            *Updates ({version}):*"""
-            f"""
-{EMOJI_ONE} Der Primebot steht jetzt auch auf *Discord* zur Verfügung! {EMOJI_PARTY2} Wir möchten die letzten beiden Spieltage nutzen, mögliche Fehler bei dieser Integration zu finden und das im Rahmen einer Closed Beta. Wenn ihr also Interesse habt, schreibt uns gerne privat (@Grayknife oder @OrbisK), damit wir euch whitelisten können.
-
-*Bugfixes:*
+Der PrimeBot steht jetzt auch bei *Discord* zu Verfügung!
 Ein Fehler wurde behoben, bei dem Teams eine falsche Spieltag-Benachrichtigung bekommen haben.
-Matches, in denen die Zeit von einem Administrator manuell eingestellt wurde, werden nun korrekt im System gespeichert und es werden nicht mehr mehrere Benachrichtigungen dazu gesendet.
-
-*Coming Soon:*
-{EMOJI_ONE} Pushnachrichten für neue Kommentare in ungespielten Matches
-
-Ein Hinweis für Teams aus dem *Swiss Starter*: Aufgrund der unregelmäßigen Veröffentlichung der wöhentlich neuen Matches ist es für uns nur begrenzt möglich, euch zeitnah Updates für diese Matches zu benachrichtigen. Wir arbeiten für den nächsten Split an einer Lösung.
-
-Wenn ihr keine Patchnotes erhalten wollt, deaktiviert die Einstellung in /settings {EMOJI_ARROW_RIGHT} Bot Patches.
-
-Sternige Grüße
-Grayknife und Orbis
+Spiele, in denen die Zeit von einem Administrator manuell eingestellt wurde, werden nun korrekt im System gespeichert und es werden nicht mehr mehrere Benachrichtigungen dazu gesendet.
 """
     },
     9: {
         "version": "1.14.1-beta",
-        "text": "Matches, in denen denen eigene Lobbies erstellt wurden, werden nun beim Crawling berücksichtigt."
+        "text": "Spiele, in denen denen eigene Lobbys erstellt wurden, werden nun beim Crawling berücksichtigt."
     },
     10: {
         "version": "1.14.2-beta",
-        "text":
-            """
-            Hallo {team.name}, 
-            
-            die Gruppenphase des aktuellen PrimeLeague-Splits ist vorbei und damit geht der Primebot bis zum nächsten Split in eine kurze Pause.
-            Die Tiebreakerspiele, die diese Woche noch stattfinden, werden von unserem Bot (noch) *nicht* berücksichtigt.
-            
-            Wir sammeln in der Pause fleißig Feedback zum Bot, zu seinen Funktionalitäten und zur Discordintegration. 🏆
-            Aus dem Grund freuen wir uns, wenn ihr uns Feedback in diesem [PL Forenpost](https://www.primeleague.gg/de/forums/1418-league-of-legends/1469-off-topic/637268) oder uns privat gebt.  
-            
-            Wir haben auf jeden Fall Bock neue geile Features zu entwickeln! 🔥
-            
-            Sternige Grüße
-            Grayknife und Orbis
-            """
+        "text": "Bugfix"
     },
     11: {
         "version": "1.15-beta",
-        "text":
-            """
-            Hallo {team.name}, 
-            """
-            f"""
-die Anmeldung für den [Summer Split 2021](https://www.primeleague.gg/de/leagues/prm/2126-summer-split-2021) hat begonnen, also let´s go!
-Mit dem Primebot startet ihr perfekt in den kommenden Split, ohne dass ihr jemals wieder etwas verpasst. 😱
-Übrigens gibt es den Primebot jetzt auch als 🔥[Discord-Bot](https://discord.com/api/oauth2/authorize?client_id=***REMOVED***&permissions=536964096&scope=bot)🔥. Nachdem ihr den Bot zum Server hinzugefügt habt, gebt einfach in einem Channel eurer Wahl `!start <TEAM-LINK>` ein. Falls ihr Hilfe benötigt, schreibt uns gerne direkt an.
-
-Sternige Grüße
-Grayknife und Orbis
-
-"""
+        "text": "Bugfix"
     },
     12: {
         "version": "1.15.1-beta",
         "text":
             """
-            Hallo {team.name}, 
-            """
-            f"""
-neben einem Hotfix für den 🔥Discord-Bot🔥 gibt es jetzt auch für diesen die Möglichkeit eine Gruppe anzugeben, sodass diese bei Benachrichtungen erwähnt wird.
-Das ganze kann auf Discord mit `!role <ROLE_NAME>` gesetzt werden.
-
-Sternige Grüße
-Grayknife und Orbis
-
+Hotfix für Discord.
+Bei Discord kann nun eine Rolle angeben werden, die bei Benachrichtigungen erwähnt wird.
 """
     },
     13: {
         "version": "1.15.2-beta",
-        "text": "Add more Changelog logic"
+        "text": "Der Changelog wurde überarbeitet."
     },
     14: {
         "version": "1.15.3-beta",
-        "text": "Hotfix Logging error"
+        "text": "Hotfix Logging Error."
     },
     15: {
         "version": "1.16",
         "text":
             """
-            Hallo {team.name}, 
-            """
-            f"""
-wir haben Neuigkiten für euch:
-
-1️⃣ Entschuldigt bitte, falls ihr in den letzten Tagen vermehrt Nachrichten zum Start der Gruppenphase mit einer **Übersicht aller Spiele** bekommen habt (oder auch gar keine bekommen habt).
-Um jedem Team nochmal die Gelegenheit zu geben, eine korrekte Übersicht ihrer Spiele zu geben, haben wir den Befehl **!overview** (Discord) bzw. **/overview** (Telegram) hinzugefügt.
-
-2️⃣ Der Primebot hat jetzt eine eigene [Website](https://www.primebot.me/)! Unter anderem findet ihr dort nochmal die ganzen Vorteile des Bots aufgelistet, wie man sein Team beim Bot registrieren kann, und mögliche Problemlösungen wenn der Bot nicht das tut, was man ihm sagt.
-Falls euch der PrimeBot also gefällt und ihr andere Teams darauf aufmerksaam machen wollt, schickt ihnen einfach den Link zur Website. :)
-
-3️⃣ Der Primebot hat mit der neuen Website auch ein neues Gesicht bekommen (eventuell habt ihr es schon bemerkt). 
-Für die Teams auf Discord gilt: Um das neue Logo zu sehen, muss der Webhook neu erstellt werden. Gebt dazu einfach **!fix** in den Chat ein.
-
-🔥Viel Erfolg in diesem Split!🔥
-Sternige Grüße
-Grayknife und Orbis
+Der Befehl `overview` wurde hinzugefügt.
+Die Website zum PrimeBot wurde online gestellt.
+Der PrimeBot wurde visuell aktualisiert.
 """,
     },
     16: {
@@ -180,35 +86,38 @@ Grayknife und Orbis
     },
     17: {
         "version": "1.17.2",
-        "text": "Added new log type lineup_fail. Added team soft delete functionality, if the group or chat does not exist anymore. Set Split to new date and added links to discord command errors. ",
+        "text": """
+Der Logtype `lineup_fail`  wurde hinzugefügt.
+Softdeletion für Teams wurde hinzugefügt, wenn die Gruppe oder der Channel nicht mehr existiert.
+"""
     },
     18: {
         "version": "1.17.3",
-        "text": "Es können jetzt Rollen mit Leerzeichen als Parameter übergeben werden.",
+        "text": "Bei Discord können jetzt Rollen mit Leerzeichen als Parameter übergeben werden.",
     },
     19: {
         "version": "1.18",
         "text":
             """
-Hallo {team.name}, 
-hier ein kurze Zusammenfassung der Neuerungen am PrimeBot.
-
-*Updates ({version}):*
-
-1️⃣ Allgemein: Ihr könnt jetzt mit !scouting (Telegram: /scouting) eine andere Scouting Website auswählen. Standardmäßig ist weiterhin OP.GG ausgewählt. Gebt uns gerne Feedback zu dem Feature oder wenn euch noch Scouting Websites fehlen.
-
-2️⃣ Discord: Es können jetzt Rollen mit Leerzeichen als Parameter übergeben werden.
-
-3️⃣ Allgemein: Ein Fehler wurde behoben, bei dem nicht alle Teams eine wöchentliche Benachrichtigung bekommen haben. Der MatchLog `lineup_fail` wird jetzt berücksichtigt. Referenzen auf Channels und Gruppen die gelöscht wurden, werden jetzt nach 1h entfernt.
-        
-Ein Hinweis für Teams aus dem *Division 1 und Pro-Division*: Derzeitig können wir noch keinen Service für diese anbieten.  
-
-Sternige Grüße
-Grayknife und Orbis
+Mit dem Befehl `scouting` kann eine andere Scouting Website ausgewählt werden. Standardmäßig ist weiterhin OP.GG ausgewählt.
+Ein Fehler wurde behoben, bei dem nicht alle Teams eine wöchentliche Benachrichtigung bekommen haben. 
+Der Spiel-Log `lineup_fail` wird jetzt berücksichtigt. 
+Referenzen auf Channels und Gruppen die gelöscht wurden, werden entfernt sobald eine Benachrichtigung gesendet werden würde.
 """
     },
     20: {
         "version": "1.18.1",
-        "text": "Overviewüberarbeitung, Fix in Bezug auf TimeChangeLog, Delete für Discord und Telegram"
+        "text": "Beim Befehl ``overview`` wurden mehr Informationen zu Spielen hinzugefügt. BugFix in Bezug auf den "
+                "TimeChangeLog. Der Befehl `delete` wurde hinzugefügt, sodass channel-/gruppenbezogene Daten "
+                "(und ggfs. Teambezogene Daten) gelöscht werden."
     },
+    21: {
+        "version": "2.0",
+        "text": """
+Die Crawler-Logik wurde entfernt und die API, die von der PrimeLeague zur Verfügung gestellt wird, wurde integriert.
+Es wurden interne Codeumstrukturierungen vorgenommen.
+Das Admininterface wurde aktiviert, um zukünftig darüber alle Daten zu administrieren.
+Es wurden Endpunkte und Logik für temporäre Einstellungslinks implementiert.
+"""
+    }
 }
