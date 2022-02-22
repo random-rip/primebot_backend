7f3b99a52e65cfc8840cea92a517ba8ebc7c302c
Janek Mangold <jmangold@singular-it.de>
2022-02-22 23:49:33 +0100

    Update season message

--- scripts/season_messages.py
+++ scripts/season_messages.py
@@ -1,6 +1,6 @@
 from app_prime_league.models import Team
 from bots.message_dispatcher import MessageDispatcher
-from bots.messages import NotificationToTeamMessage
+from bots.messages import NotificationToTeamMessage, MatchesOverview
 
 season_end_message = """
 Hallo {team.name}, 
@@ -31,19 +31,9 @@ Grayknife und Orbis
 message = """
 Hallo {team.name}, 
 
-der PrimeBot wurde auf Version v2.0.0 aktualisiert. 
-Mit dieser Version kommen ein paar neue Features und Überarbeitungen des Cores auf das Produktivsystem.
+ihr spielt aktuell in der Swiss Starter Division. Leider wurden die Matches von euch nicht korrekt aktualisiert. Deswegen kann es sein, dass dafür keine Benachrichtigungen angekommen sind. 
+Die Matches wurden jetzt erfolgreich aktualisiert, jedoch solltet ihr noch nachschauen, ob es beispielsweise Terminvorschläge vom Gegner gibt.
 
-Hauptänderungen:
-- Anbindung der Prime League API
-- Deployment unserer Website und Personalisierung von Benachrichtigungen mit `!settings` (Telegram `/settings`).
-- Wichtig: Wir haben alle Benachrichtigungseinstellungen zurückgesetzt (ging nicht anders, sorry).
-- Implementierung von `!match [match_day]` um eine detaillierte Übersicht des Spieltages zu generieren.
-- ...noch vieles mehr! Alle Aktualisierungen findet ihr [hier](https://primebot.me/changelogs).
-
-Außerdem freuen wir uns euch mitteilen zu dürfen, dass wir nun deutlich häufiger die Daten der Prime League abgreifen dürfen.
-
-Wir wünschen euch viel Erfolg beim restlichen Split! 
 
 Sternige Grüße
 Grayknife, Orbis & Mörlin
@@ -51,17 +41,17 @@ Grayknife, Orbis & Mörlin
 
 
 def main():
-    teams = Team.objects.get_registered_teams()
+    teams = Team.objects.get_registered_teams().filter(division="Swiss Starter")
     for team in teams:
         try:
             print(team)
             dispatcher = MessageDispatcher(team)
             msg = NotificationToTeamMessage(team=team, custom_message=message)
             dispatcher.dispatch_raw_message(msg=msg)
+            dispatcher.dispatch(MatchesOverview, )
         except Exception as e:
             print(e)
 
 
-# python manage.py runscript debug
 def run():
     main()
