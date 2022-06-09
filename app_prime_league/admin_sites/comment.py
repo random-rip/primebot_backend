e457d67862a65a7de82ef8022fe3bdde112c2221
Janek Mangold <jmangold@singular-it.de>
2022-06-09 19:53:56 +0200

    fix

--- app_prime_league/admin_sites/comment.py
+++ app_prime_league/admin_sites/comment.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'match', 'comment_parent_id', 'content', 'user_id', 'created_at', 'updated_at',
+    list_display = ['id', "comment_id", 'match', 'comment_parent_id', 'content', 'user_id', 'created_at', 'updated_at',
                     "comment_edit_user_id", "comment_flag_staff", "comment_flag_official"]
     list_filter = ['created_at', 'updated_at', "comment_edit_user_id"]
     readonly_fields = ("created_at", "updated_at",)
