d1313cee84c974f4142cf085f88c0a75437c4947
Janek Mangold <janek.mangold@web.de>
2022-09-27 00:22:44 +0200

    Set utf8mb4 as db encoding

--- primebot_backend/settings.py
+++ primebot_backend/settings.py
@@ -113,7 +113,8 @@ DATABASES = {
         'PORT': env.str('DB_PORT'),
         'CONN_MAX_AGE': 3600,
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
         }
     }
 }
