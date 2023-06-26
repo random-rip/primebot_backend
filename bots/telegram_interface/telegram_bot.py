f48d12854dee068e1d5d718aa5fdcdebfc8fa292
Grayknife <75701163+Grayknife@users.noreply.github.com>
2023-06-26 21:07:39 +0200

    Fix: Telegram 400 chat not found (#52)
    Fix: Catch 400 error for Telegram messages
    Fix: Remove unused python file

--- bots/telegram_interface/telegram_bot.py
+++ bots/telegram_interface/telegram_bot.py
@@ -3,9 +3,9 @@ import sys
 import traceback
 
 from django.conf import settings
-from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackQueryHandler
+from telegram.ext import CallbackQueryHandler, CommandHandler, ConversationHandler, MessageHandler, Updater
 from telegram.ext.filters import Filters
-from telepot.exception import BotWasKickedError, BotWasBlockedError
+from telepot.exception import BotWasBlockedError, BotWasKickedError, TelegramError
 
 from bots import send_message
 from bots.base.bot_interface import BotInterface
@@ -26,13 +26,7 @@ class TelegramBot(BotInterface):
     def __init__(self):
         if not settings.TELEGRAM_BOT_KEY:
             raise VariableNotSetException("TELEGRAM_BOT_KEY")
-        super().__init__(
-            bot=Updater,
-            bot_config={
-                "token": settings.TELEGRAM_BOT_KEY,
-                "use_context": True
-            }
-        )
+        super().__init__(bot=Updater, bot_config={"token": settings.TELEGRAM_BOT_KEY, "use_context": True})
 
     def _initialize(self):
         dp = self.bot.dispatcher
@@ -45,17 +39,22 @@ class TelegramBot(BotInterface):
             CommandHandler("matches", single_commands.matches),
             CommandHandler("delete", single_commands.delete),
             CommandHandler("settings", single_commands.team_settings),
-            MessageHandler(Filters.status_update.migrate, single_commands.migrate_chat)  # Migration
+            MessageHandler(Filters.status_update.migrate, single_commands.migrate_chat),  # Migration
         ]
 
         start_conv_handler = ConversationHandler(
-            entry_points=[CommandHandler('start', start_conversation.start, )],
-
+            entry_points=[
+                CommandHandler(
+                    'start',
+                    start_conversation.start,
+                )
+            ],
             states={
-                1: [MessageHandler(Filters.text & (~Filters.command), start_conversation.team_registration), ],
+                1: [
+                    MessageHandler(Filters.text & (~Filters.command), start_conversation.team_registration),
+                ],
             },
-
-            fallbacks=commands
+            fallbacks=commands,
         )
 
         # Allgemeine Commands
@@ -83,13 +82,15 @@ class TelegramBot(BotInterface):
         """
         try:
             send_message(msg=msg.generate_message(), chat_id=team.telegram_id, raise_again=True)
-        except (BotWasKickedError, BotWasBlockedError) as e:
+        except (BotWasKickedError, BotWasBlockedError, TelegramError) as e:
+            if isinstance(e, TelegramError) and not e.description == 'Bad Request: chat not found':
+                notifications_logger.exception(e)
+                raise e
             team.set_telegram_null()
             notifications_logger.info(f"Soft deleted Telegram {team}'")
             return
         except Exception as e:
-            notifications_logger.error(
-                f"Could not send Telegram Message {msg.__class__.__name__} to {team}.")
+            notifications_logger.error(f"Could not send Telegram Message {msg.__class__.__name__} to {team}.")
             raise e
 
 
@@ -97,14 +98,18 @@ def error(update, context):
     try:
         trace = "".join(traceback.format_tb(sys.exc_info()[2]))
 
-        text = f"The error <code>{context.error}</code> happened in one of the telegram chats.\n" \
-               f"Full trace: <code>{trace}</code>"
+        text = (
+            f"The error <code>{context.error}</code> happened in one of the telegram chats.\n"
+            f"Full trace: <code>{trace}</code>"
+        )
         notifications_logger.exception(trace)
         send_message_to_devs(text)
 
         if update and update.effective_message:
-            text = "Hey, es ist ein unerwarteter Fehler aufgetreten, während ich euren Befehl verarbeiten wollte. " \
-                   "Bitte kontaktiert die Programmierer über Discord oder Telegram."
+            text = (
+                "Hey, es ist ein unerwarteter Fehler aufgetreten, während ich euren Befehl verarbeiten wollte. "
+                "Bitte kontaktiert die Programmierer über Discord oder Telegram."
+            )
             update.effective_message.reply_text(text)
     except Exception as e:
         text = f"Ein gravierender Fehler ist aufgetreten.\n{e}"
