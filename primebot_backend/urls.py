7f9d37fd1c8b58dec47825d5d1e7cded2a71e4d2
Janek Mangold <jmangold@singular-it.de>
2023-10-05 16:25:37 +0200

    Set urls back to admin

--- primebot_backend/urls.py
+++ primebot_backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('.admin/', admin.site.urls, name="admin"),
+    path('admin/', admin.site.urls, name="admin"),
     path('api/', include('app_api.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
