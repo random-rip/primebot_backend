33a68a1e9640f71ef21846b2a24b03425a139e68
Janek Mangold <jmangold@singular-it.de>
2023-10-07 16:10:47 +0200

    Include queryparams in redirect, Remove log

--- app_api/urls.py
+++ app_api/urls.py
@@ -6,9 +6,9 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def version_v1_redirect(request, *args, **kwargs):
     url = "/api/v1/" + request.path.lstrip("/api/")
-    args = request.META.get('QUERY_STRING', '')
-    if args:
-        url += f"?{args}"
+    query_string = request.META.get('QUERY_STRING', '')
+    if query_string:
+        url += f"?{query_string}"
     return redirect(to=url, *args, permanent=True, **kwargs)
 
 
