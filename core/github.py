383b8e84caebb03167ef91866d8b6a3fd4f95dcf
Janek Mangold <jmangold@singular-it.de>
2023-10-07 16:44:14 +0200

    Fix github 403 error

--- core/github.py
+++ core/github.py
@@ -3,6 +3,16 @@ import logging
 import requests
 from django.core.cache import cache
 
+logger = logging.getLogger("django")
+
+
+class GitHubException(Exception):
+    pass
+
+
+class RateLimitedException(GitHubException):
+    pass
+
 
 class GitHub:
     BASE_URL = "https://api.github.com/repos/random-rip/primebot_backend"
@@ -12,21 +22,29 @@ class GitHub:
 
     @classmethod
     def get_json(cls, url):
-        data = requests.get(url).json()
-        return data
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        if response.status_code == 403:
+            raise RateLimitedException(f"Github Rate limit: {response.json()}")
+        else:
+            raise GitHubException(
+                f"GitHub API returned status code {response.status_code}. Content: f{response.json()}"
+            )
 
     @classmethod
     def releases(cls):
         cached = cache.get(cls.RELEASES_CACHE_KEY)
         if cached:
-            logging.getLogger("django").warning("Using cached releases")
+            logger.debug("Using cached releases")
             return cached
         try:
-            logging.getLogger("django").warning("Fetching releases")
+            logger.debug("Fetching releases")
             data = cls.get_json(cls.RELEASES)
             cache.set(cls.RELEASES_CACHE_KEY, data, cls.CACHE_DURATION)
             return data
-        except Exception:
+        except Exception as e:
+            logger.error("Error fetching releases: " + str(e))
             return []
 
     @classmethod
@@ -38,18 +56,19 @@ class GitHub:
             "body": None,
         }
         if cached:
-            logging.getLogger("django").warning("Using cached latest release")
+            logger.debug("Using cached latest release")
             ret["version"] = cached[0].get("tag_name", None)
             ret["released_at"] = cached[0].get("published_at", None)
             ret["body"] = cached[0].get("body", None)
             return ret
         try:
-            logging.getLogger("django").warning("Fetching latest release")
+            logger.debug("Fetching latest release")
             data = cls.get_json(cls.RELEASES)
             cache.set(cls.RELEASES_CACHE_KEY, data, cls.CACHE_DURATION)
             ret["version"] = data[0].get("tag_name", None)
             ret["released_at"] = data[0].get("published_at", None)
             ret["body"] = data[0].get("body", None)
             return ret
-        except Exception:
+        except Exception as e:
+            logger.error(e)
             return ret
