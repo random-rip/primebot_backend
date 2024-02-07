7fb66b8e360f36e5b1530cd0fb7f7456ae6cadf7
Janek Mangold <janek.mangold@web.de>
2024-02-07 22:16:31 +0100

    wip

--- app_api/api_v1/modules/teams/views.py
+++ app_api/api_v1/modules/teams/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from django_ical.views import ICalFeed
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
 
@@ -41,3 +42,25 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
         if self.action == 'retrieve':
             return self.detail_serializer_class
         return self.serializer_class
+
+
+class TeamMatchesFeed(ICalFeed):
+    """
+    A simple event calender
+    """
+
+    product_id = '-//primebot.me//team-matches//EN'
+    timezone = 'UTC'
+    file_name = "event.ics"
+
+    def items(self):
+        return self.team.matches_against.objects.all().order_by('-start_datetime')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+    def item_start_datetime(self, item):
+        return item.start_datetime
