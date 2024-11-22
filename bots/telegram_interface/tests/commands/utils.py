43469f4a06430f019ac251540edea9eac44c5576
Janek Mangold <janek.mangold@singular-it.de>
2024-11-22 16:05:18 +0100

    chore: Replace telepot with python-telegram-bot, Update tests, Update Django-Admin-Interface

--- bots/telegram_interface/tests/commands/utils.py
+++ bots/telegram_interface/tests/commands/utils.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+from unittest.mock import MagicMock
+
 from telegram import Chat, Message, Update
 
 from bots.telegram_interface.commands.single_commands import call_match
 
 
-class TestBot:
+class BotMock(MagicMock):
     response_text: str = ''
 
     def send_message(self, chat_id, *args, **kwargs):
@@ -14,8 +17,9 @@ class TestChat(Chat):
     type = Chat.CHANNEL
 
 
-def test_call_match(text: str, chat, bot):
-    message = Message(1, 1, None, chat, text=text, bot=bot)
+def test_call_match(text: str, chat=None, bot=None):
+    chat = chat or Chat(1, Chat.CHANNEL)
+    bot = bot or BotMock()
+    message = Message(1, chat=chat, text=text, bot=bot, date=datetime.now())
     update = Update(1, message)
-
-    call_match(update, None)
+    call_match(update, MagicMock())
