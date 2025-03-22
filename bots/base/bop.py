291243761da9a2510859476162f26d98b83cf037
Janek Mangold <janek.mangold@singular-it.de>
2025-03-22 17:21:13 +0100

    feat(discord): Add choice parameter to bop

--- bots/base/bop.py
+++ bots/base/bop.py
@@ -1,5 +1,6 @@
 import random
 from abc import abstractmethod
+from typing import Iterable
 
 import niquests
 
@@ -7,13 +8,17 @@ from bots.telegram_interface.tg_singleton import send_message_to_devs
 
 
 class AnimalAPI:
+    animal = None
+
     @classmethod
     @abstractmethod
-    def get_url(cls):
-        return
+    def get_url(cls) -> str:
+        pass
 
 
 class CatAPI(AnimalAPI):
+    animal = 'cat'
+
     @classmethod
     def get_url(cls):
         url = 'https://cataas.com/cat/gif'
@@ -21,6 +26,8 @@ class CatAPI(AnimalAPI):
 
 
 class DogAPI(AnimalAPI):
+    animal = 'dog'
+
     @classmethod
     def get_url(cls):
         contents = niquests.get('https://api.thedogapi.com/v1/images/search?mime_types=gif').json()
@@ -28,14 +35,27 @@ class DogAPI(AnimalAPI):
         return url
 
 
-class GIFinator:
-    apis = [CatAPI, DogAPI]
+class Gifinator:
+    apis: dict[str, AnimalAPI] = {api.animal: api for api in [CatAPI, DogAPI]}
 
-    @staticmethod
-    def get_gif(team=None):
-        animal = random.choice(GIFinator.apis)
+    @classmethod
+    def animals(cls) -> Iterable[str]:
+        return cls.apis.keys()
+
+    @classmethod
+    def get_gif(cls, animal=None) -> str:
+        api = cls.get_api(animal)
         try:
-            return animal.get_url()
+            return api.get_url()
         except Exception as e:
             send_message_to_devs("bop raised an exception:", code=str(e))
             raise ConnectionError("Not accessible")
+
+    @classmethod
+    def get_api(cls, animal=None):
+        if animal is None:
+            return random.choice(list(cls.apis.values()))
+        try:
+            return cls.apis[animal]
+        except KeyError:
+            raise ValueError("No such animal")
